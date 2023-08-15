4c916eaf6b10ecd46c422521b2799777b7ed4f14
Axlh15 <axlh15@gmail.com>
2023-08-15 15:42:51 -0500

    VS clasesiniciales
    Práctica de clase, dispositivos moviles,

---
+++ ClasesInicialesProgra/ClasesInicialesProgra/DispositivoMovil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//El namespace es un agrupador de clases y permite organizarlas
+namespace ClasesInicialesProgra
+{
+    public class DispositivoMovil
+    {
+        public int ram;
+        public int almacenamiento;
+        public float tamaño;
+        public int ResoluAncho;
+        public int resoluAlto;
+        public Color color;
+    }
+}
---
+++ ClasesInicialesProgra/ClasesInicialesProgra/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesInicialesProgra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Punto de iniciio o de entrada de mi programa
+            //Tipo            identificador inicialización
+            DispositivoMovil miIphone = new DispositivoMovil(); //declaración e instanciación
+            miIphone.almacenamiento = 64;
+            miIphone.ram = 16;
+            miIphone.resoluAlto = 1080;
+            miIphone.ResoluAncho = 1920;
+            miIphone.color = new Color(); //instanciación sin declaración
+            miIphone.color.rojo = 100;
+            miIphone.color.verde = 20;
+            miIphone.color.verde = 210;
+
+            DispositivoMovil tabletSamsung = new DispositivoMovil();
+            tabletSamsung.color = 256;
+            tabletSamsung.almacenamiento = 256;
+            tabletSamsung.ram = 8;
+            tabletSamsung.resoluAlto = 1080;
+            tabletSamsung.ResoluAncho = 1920;
+
+            DispositivoMovil Huaweiphone = new DispositivoMovil();
+            
+            Huaweiphone.almacenamiento = 500;
+            Huaweiphone.ram = 16;
+            Huaweiphone.resoluAlto = 1080;
+            Huaweiphone.ResoluAncho = 1920;
+            Huaweiphone.color = new Color();
+            Huaweiphone.color.rojo = 50;
+            Huaweiphone.color.verde = 100;
+            Huaweiphone.color.azul = 37;
+        }
+    }
+}
