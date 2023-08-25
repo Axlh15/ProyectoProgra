735f50d01e6471eb5f6642f7113ca11600881946
Axlh15 <142038801+Axlh15@users.noreply.github.com>
2023-08-24 22:16:38 -0600

    Carrera de coches terminada

---
+++ ClasesInicialesProgra/CarreraCoches/PistaCarreras.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarreraCoches
+{
+    public class PistaCarreras
+    {
+        public string nombre;
+        public float longitud;
+
+        //metodo que recibe dos parametros
+        public void Competir (Auto auto1, Auto auto2)
+        { //bloque del metodo
+
+            Console.WriteLine("Iniciando competencia entre" + auto1.modelo + " contra " + auto2.modelo);
+
+            if (auto1.velocidad > auto2.velocidad)
+            {
+                Console.WriteLine("Ganó" + auto1.modelo);
+            }
+            else if (auto2.velocidad > auto1.velocidad)
+            {
+                Console.WriteLine("Ganó" + auto2.modelo);
+            }
+            else
+            {
+                Console.WriteLine("Hubo un empate");
+            }
+        }
+    }
+}
---
+++ ClasesInicialesProgra/CarreraCoches/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarreraCoches
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto ferrari = new Auto();
+            ferrari.modelo = " FerrariF1 ";
+            ferrari.velocidad = 120;
+
+            Auto mclaren = new Auto();
+            mclaren.modelo = " MC 2443 ";
+            mclaren.velocidad = 140; //km/h
+
+            PistaCarreras pista1 = new PistaCarreras();
+            pista1.nombre = " Hermanos Rodriguez ";
+            pista1.longitud = 4.303f; //km
+
+            pista1.Competir(ferrari, mclaren);
+
+            //esta linea espera que presiones enter para continuar
+            Console.ReadLine();
+        }
+    }
+}
