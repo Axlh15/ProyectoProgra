228e77e3ab90cd90361026dfe851af6d85c08aad
Axlh15 <axlh15@gmail.com>
2023-09-12 15:42:21 -0500

    unity cavernicola
    cambio de version a 2022

--- Plataformero/Assets/Scripts/ControladorJugador.cs
+++ Plataformero/Assets/Scripts/ControladorJugador.cs
@@ -4,20 +4,41 @@ using UnityEngine;
 
 public class ControladorJugador : MonoBehaviour
 {
-    private Rigidbody2D MiCuerpo;
-    private Animator MiAnimador;
+    public float velocidadCaminar = 5;
+    private Rigidbody2D miCuerpo;
+    private Animator miAnimador;
     // Start is called before the first frame update
     void Start()
     {
-        MiCuerpo = GetComponent<Rigidbody2D>();
-        MiAnimador = GetComponent<Animator>();
+        miCuerpo = GetComponent<Rigidbody2D>();
+        miAnimador = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float velVert = MiCuerpo.velocity.y;
-        MiCuerpo.velocity = new Vector3(3, velVert, 0);
-        MiAnimador.SetBool("CAMINANDO", true);
+        float velVert = miCuerpo.velocity.y;
+
+        float movHoriz = Input.GetAxis("Horizontal");
+
+        if (movHoriz > 0)
+        {//se mueve a la derecha
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            miCuerpo.velocity = new Vector3(velocidadCaminar, velVert, 0);
+            miAnimador.SetBool("CAMINANDO", true);
+        }
+        else if (movHoriz < 0)
+        {//izquierda
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            miCuerpo.velocity = new Vector3(-velocidadCaminar, velVert, 0);
+            miAnimador.SetBool("CAMINANDO", true);
+        }
+        else
+        {//no se mueve
+            miCuerpo.velocity = new Vector3(0, velVert, 0);
+            miAnimador.SetBool("CAMINANDO", false);
+        }
+
+        
     }
 }
