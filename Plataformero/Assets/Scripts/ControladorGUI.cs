6d8bdf590bcbbed94451c365a506524e924dab4a
Axlh15 <axlh15@gmail.com>
2023-11-10 11:54:31 -0600

    item caja, no pude

--- Plataformero/Assets/Scripts/ControladorGUI.cs
+++ Plataformero/Assets/Scripts/ControladorGUI.cs
@@ -17,6 +17,7 @@ public class ControladorGUI : MonoBehaviour
     public Personaje enemigo;
     public GameObject GameOver;
 
+
     // Start is called before the first frame update
     void Start()
     {
