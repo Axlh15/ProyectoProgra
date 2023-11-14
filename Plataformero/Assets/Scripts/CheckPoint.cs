1c9834f66c7474b0f5cfe74590c35ad2c5425711
Axlh15 <axlh15@gmail.com>
2023-11-14 13:47:37 -0600

    checkpoint
    me revisò las tareas que faltaban y queda pendiente el checkpoint y la tarea de meta

---
+++ Plataformero/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        miPersonaje = GetComponent<Personaje>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()
+    }
+}
