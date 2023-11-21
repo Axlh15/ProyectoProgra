d904e69f480462761ec6c43ed9cece7ee3ec4de1
Axlh15 <axlh15@gmail.com>
2023-11-21 14:51:07 -0600

    efectos luz

--- Plataformero/Assets/Scripts/CheckPoint.cs
+++ Plataformero/Assets/Scripts/CheckPoint.cs
@@ -7,12 +7,12 @@ public class CheckPoint : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        miPersonaje = GetComponent<Personaje>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if()
+       
     }
 }
