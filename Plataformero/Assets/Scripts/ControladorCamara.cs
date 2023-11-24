8aa55a319b11a10513ad8c9dca1d1c82cfad92fa
Axlh15 <142038801+Axlh15@users.noreply.github.com>
2023-11-24 05:26:45 -0600

    tarea
    luces

--- Plataformero/Assets/Scripts/ControladorCamara.cs
+++ Plataformero/Assets/Scripts/ControladorCamara.cs
@@ -5,20 +5,15 @@ using UnityEngine;
 public class ControladorCamara : MonoBehaviour
 {
     public Personaje cavernicola;
+    public float velocidadC = 3f;
+    public Transform target;
    
 
     // Update is called once per frame
     void Update()
     {
 
-        bool puedoMoverme = cavernicola.estaVivo() && !cavernicola.bloqueado;
-
-        if (puedoMoverme)
-        {
-            transform.position = new Vector3(
-            cavernicola.transform.position.x, //la pos x de la camara   
-            cavernicola.transform.position.y, // la pos y  
-             -1); //la pos z
-        }
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, velocidadC * Time.deltaTime);
     }
 }
